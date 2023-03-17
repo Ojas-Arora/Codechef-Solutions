d2ab80c56692428e3a90d0bed4613ed8cae7da90
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-03-18 00:41:50 +0530

    Add files via upload

---
+++ PERFECTTRIO.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int T;
+cin>>T;
+while (T--){
+    int A,B,C;
+    cin>>A>>B>>C;
+        if ((A+B)==C ||  (B+C)==A ||  (C+A)==B)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
