a45754ed266bb50b32c516d302a06d42a3a7647a
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-03-18 00:29:48 +0530

    Add files via upload

---
+++ AGELIMIT.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  	int T,X,Y,A,i;
+	cin>>T;
+	
+	for(i=0;i<T;i++)
+	{
+	    cin>>X>>Y>>A;
+	    
+	    if(X<=A && Y>A)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
+	
