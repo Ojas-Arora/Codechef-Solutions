29c2e520d994c7777ae3142a69f71074d69b0505
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-04-26 21:39:28 +0530

    Add files via upload

---
+++ TENPACKETS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+while(t--)
+{
+     int num1,num2;
+     cin>>num1>>num2;
+     cout<<num2*2+num1*1<<endl;
+}
+return 0;
+}
+
+	
